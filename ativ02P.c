b39a0396ddbe7e50f5d0c270b756242093b87bdc
izadnz <10744968@mackenzista.com.br>
2025-08-13 01:31:12 +0000

    adicionado ano FCI

--- ativ02P.c
+++ ativ02P.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main()  {
-	printf("Mackenzie - FCI\n");			
+	printf("Mackenzie - FCI 1970\n");			
 	printf("Algoritmos e ");
 	printf("Prog. II\n");
 	printf("Ciencia da computacao\n");
